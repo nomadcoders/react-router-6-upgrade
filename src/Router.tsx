171a258b0e601d16d9987414f583ef090ca70136
니꼬 <nico@nomadcoders.co>
2022-10-21 17:50:43 +0900

    errorElement

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import About from "./screens/About";
 import Home from "./screens/Home";
 import Root from "./Root";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
 
 const router = createBrowserRouter([
   {
@@ -11,12 +13,14 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+        errorElement: <ErrorComponent />,
       },
       {
         path: "about",
         element: <About />,
       },
     ],
+    errorElement: <NotFound />,
   },
 ]);
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,5 @@
 function Home() {
-  return <h1>Home</h1>;
+  const users: any = [];
+  return <h1>{users[0].name}</h1>;
 }
 export default Home;
